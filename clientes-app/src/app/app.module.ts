d2f9770dd336088a85304b39458d250989f3f435
marciomiyamoto <marcio_takashi@hotmail.com>
2021-02-22 23:23:39 -0300

    Criando template inicial do cliente-app

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { CursoAngularComponent } from './curso-angular/curso-angular.component';
-import { TesteComponent } from './teste/teste.component'
-import { ClientesModule } from './clientes/clientes.module';
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    CursoAngularComponent,
-    TesteComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ClientesModule
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
